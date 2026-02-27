46cf3c7e484ba8c6441399c520640425760b24d4
Duy Nam Tong <145951925+nomidevc@users.noreply.github.com>
2026-02-27 12:29:47 +0700

    feat(gameplay): Add Cinemachine scroll zoom
    Implement mouse-scroll zoom using the CinemachineTransposer's m_FollowOffset.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    
+    [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+
+    private CinemachineTransposer m_cinemachineTransposer;
+    private const float MIN_FOLLOW_OFFSET_Y = 2f;
+    private const float MAX_FOLLOW_OFFSET_Y = 15f;
+    
     void Update()
     {
         Vector3 inputVector = new Vector3(0, 0, 0);
@@ -40,5 +48,22 @@ public class CameraController : MonoBehaviour
         }
         float rotationSpeed = 50f;
         transform.eulerAngles += rotationVector * (Time.deltaTime * rotationSpeed);
+        
+        m_cinemachineTransposer = _virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        Vector3 zoomVector = m_cinemachineTransposer.m_FollowOffset;
+        float zoomAmount = 1f;
+        if(Input.mouseScrollDelta.y > 0)
+        {
+            zoomVector.y -= zoomAmount;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            zoomVector.y += zoomAmount;
+        }
+        zoomVector.y = Mathf.Clamp(zoomVector.y, MIN_FOLLOW_OFFSET_Y, MAX_FOLLOW_OFFSET_Y);
+        float zoomLerpSpeed = 50f;
+        m_cinemachineTransposer.m_FollowOffset = 
+            Vector3.Lerp(m_cinemachineTransposer.m_FollowOffset, zoomVector, Time.deltaTime * zoomLerpSpeed);
+
     }
 }
